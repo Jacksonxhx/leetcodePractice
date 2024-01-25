39e980e2fe0973484fc055116bd5f70157d2efdc
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-01-25 18:53:44 -0500

    Time: 98 ms (46.87%), Space: 19.4 MB (97.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,27 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        position = dict()
-        used = []
-        tmp = []
-        for word in strs:
-            sorted_word = sorted(word)
-            tmp.append(''.join(sorted_word))
-            
-        for i in range(len(tmp)):
-            if tmp[i] in used: continue
-            else: used.append(tmp[i])
-            for j in range(len(tmp)):                
-                if tmp[i] == tmp[j]:
-                    if tmp[i] in position:
-                        position[tmp[i]].append(j)
-                    else:
-                        position[tmp[i]] = [j]
-        res = []         
-        for key, value in position.items():
-            tmp = [strs[i] for i in value]
-            res.append(tmp)
+        anagrams = dict()
         
-        return res
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
             
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+        
+        return list(anagrams.values())
         
         
