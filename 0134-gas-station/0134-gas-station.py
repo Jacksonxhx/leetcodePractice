c5428f4daa2d868fae3d1548ef00ea7a2a59f34b
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-04-04 20:39:25 -0400

    Time: 918 ms (90.35%), Space: 22 MB (53.76%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -11,13 +11,10 @@ class Solution:
         for i in range(size):
             totalGas += gas[i] - cost[i]
             remGas += gas[i] - cost[i]
-            # If remGas is negative, it means start from 'i' cannot reach 'i + 1'.
-            # So, we need to try starting from 'i + 1', and reset remGas to 0.
+            # 如果remGas小于0，意味着走不到
             if remGas < 0:
                 start = i + 1
                 remGas = 0
 
-        # After trying all starts, if totalGas is still >= 0, it means the circuit can be completed.
-        # The start point found will be the correct starting position.
-        # If totalGas is negative, it means it's impossible to complete the circuit.
-        return start if totalGas >= 0 else -1
+        # 最后如果totalGas还是
+        return start 
