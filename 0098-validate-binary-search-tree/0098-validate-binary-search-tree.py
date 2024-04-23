dee664eb44dd3e7a7ec8c926d1f377ee68563710
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-04-23 18:59:21 -0400

    Time: 39 ms (73.69%), Space: 18.3 MB (83.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -8,13 +8,17 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
         def prefix(root, min_v, max_v):
+            # Base case
             if root == None:
                 return True
             
+            # 列出所有错的情况
             if root.val >= max_v or root.val <= min_v:
                 return False
             
+            # 判断
             return prefix(root.left, min_v, root.val) and prefix(root.right, root.val, max_v)
         
+        # min是无限小，max是无限大
         return prefix(root, float('-inf'), float('inf'))
             
