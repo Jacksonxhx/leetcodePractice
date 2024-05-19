4fc48af79120a0a276c188c3fda7718b432a4bc0
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-05-19 17:03:30 +0800

    Time: 34 ms (79.35%), Space: 16.9 MB (42.55%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,26 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        '''
+        根据symbol在不同index中决定选择'('还是')'填入
+        '''
         res = []
         path = []
         
         def backtracking(symbol, index):
             # base case, 每当path满之后加入res
             if 2 * n == index:
+                # 需要确定'('和')'的数量一样
                 if symbol == 0:
                     res.append("".join(path))
                     return
+            # 分类分情况讨论
             else:
+                # 当'('的数量小于n个时append
                 if symbol < n:
                     path.append('(')
                     backtracking(symbol + 1, index + 1)
                     path.pop()
+                # 当')'的数量小于'('的数量时
                 if symbol > 0:
                     path.append(')')
                     backtracking(symbol - 1, index + 1)
