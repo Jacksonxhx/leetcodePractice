56b94425fad64d1907d7e9d05bb4c956470f85b8
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-05-22 12:59:12 +0800

    Time: 40 ms (55.76%), Space: 16.5 MB (89.60%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,16 +3,21 @@ class Solution:
         '''
         用二分查找找到对应的值
         '''
-        left, right = 0, x
+        if x < 2:
+            return x
         
-        while left < right:
+        # 因为任何大于 1 的数的平方根不可能大于其一半
+        left, right = 2, x // 2
+        
+        while left <= right:
             mid = (left + right) // 2
+            num = mid * mid
             
-            if mid * mid == x or (mid * mid < x and (mid+1) * (mid+1) > x):
+            if num == x:
                 return mid
-            elif mid * mid > x:
-                right = mid - 1
-            else:
+            elif num < x:
                 left = mid + 1
+            else:
+                right = mid - 1
         
-        return left
+        return right
