f2c65689d4cbd982db051ae8e301c1200e20ff63
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-05-22 12:08:20 +0800

    Time: 35 ms (69.12%), Space: 16.5 MB (84.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,16 +1,7 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        strd = ''
-        for i in digits:
-            strd += str(i)
-        num = int(strd)
-        num += 1
-        
-        res = []
-        
-        num = str(num)
-        
-        for i in num:
-            res.append(int(i))
-            
-        return res
+        # map函数依次把digits的内容call str()函数
+        num = int(''.join(map(str, digits))) + 1
+
+        # 将结果转换为字符串后再拆分为单个数字并存入列表
+        return [int(i) for i in str(num)]
