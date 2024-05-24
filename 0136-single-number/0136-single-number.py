97bf1062d091455a5e093f2ba2d6fbeeb2549ca4
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-05-24 23:30:55 +0800

    Time: 109 ms (65.11%), Space: 19.1 MB (72.82%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         '''
-        先排序然后看前后是否相等
+        用位运算的XOR做
         '''
+        if len(nums) == 1:
+            return nums[0]
         
-        nums = sorted(nums)
-
-        i = 0
-        while i < len(nums) - 1:
-            j = i + 1
-            if nums[i] == nums[j]:
-                i += 2
-            else:
-                return nums[i]
+        ans = 0
+        for i in nums:
+            ans ^= i
         
-        return nums[i]
+        return ans
