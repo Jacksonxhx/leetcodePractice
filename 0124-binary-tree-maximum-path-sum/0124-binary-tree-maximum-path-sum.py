1ea47e74293d11b5cd3696505f4981adf26dec5b
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-05-31 00:35:28 +0800

    Time: 62 ms (66.80%), Space: 20.7 MB (93.36%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,23 +6,30 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        '''
+        DP的思想，DFS可以做，其实就是求每个root下最大的sum
+        每个node的最大和就是由于下面两个最大的和而来的，如果是负数，这个node自身的最大值就是0
+        但是如果这个node想要perform左+右，就不能为0，为0只有左右选一遍的时候才可以
+        '''
+        # 定义全局变量
         self.max_sum = float('-inf')
         self.dfs(root)
         return self.max_sum
     
     def dfs(self, node):
+        # base case
         if not node:
             return 0
         
-        # Recursively calculate the maximum sum on the left and right subtrees
+        # recursion call，如果小于0则最大是0（不带上这个node）
         left_max = max(self.dfs(node.left), 0)
         right_max = max(self.dfs(node.right), 0)
         
-        # Calculate the path sum that includes the current node
+        # 对于当前这个node的最大值
         current_sum = node.val + left_max + right_max
         
-        # Update the global maximum path sum
+        # update global value
         self.max_sum = max(self.max_sum, current_sum)
         
-        # Return the maximum path sum where the current node is the endpoint
+        # 返回当前路线的最大值，因为是path所以不能分叉，只能传一边的
         return node.val + max(left_max, right_max)
