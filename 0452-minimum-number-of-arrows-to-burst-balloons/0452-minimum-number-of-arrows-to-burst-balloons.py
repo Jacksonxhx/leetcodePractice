b0083b787fe2c3e2be6915e08729fd328d46892d
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-07 02:06:36 +0800

    Time: 1056 ms (54.04%), Space: 62.6 MB (81.61%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        '''
+        贪心：寻找区间重叠最多的地方
+        按照points结束的位置升序排列，避免一个range包括了很多小的range
+        当坐标小于区间起始位置，count ++
+        '''
+        if not points:
+            return 0
+        
+        # 按照结束生序排列
+        points.sort(key=lambda x: x[1])
+        count = 1
+        arrow_pos = points[0][1]
+        
+        for i in range(1, len(points)):
+            if points[i][0] > arrow_pos:
+                count += 1
+                arrow_pos = points[i][1]
+        
+        return count
