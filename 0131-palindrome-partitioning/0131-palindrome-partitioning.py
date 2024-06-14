b765cec4dcac2749748b9b020de259085e9df4a6
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-15 00:44:01 +0800

    Time: 467 ms (50.44%), Space: 35.1 MB (59.40%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,28 +1,33 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        """
+        回溯算法找s所有对应的substring然后判断是否回文
+        """
+        # 初始化res
         res = []
-        partition = []
+        path = []
         
         def backtracking(start: int):
-            # 用start当作index记录
             if start == len(s):
-                # 要append[:]，是副本而不是地址
-                res.append(partition[:])
+                res.append(path[:])
                 return
             
             # 用end记录单一palindrome substring的end
+            # 枚举所有的可能性
             for end in range(start + 1, len(s) + 1):
+                # 给出判断条件
                 if self.is_palindrome(s[start:end]):
-                    partition.append(s[start:end])
-                    # 递归
-                    backtracking(end)
+                    # 满足条件，加入path
+                    path.append(s[start:end])
                     # 回溯
-                    partition.pop()
+                    backtracking(end)
+                    # 清理现场
+                    path.pop()
         
         backtracking(0)
         return res
     
-    def is_palindrome(self, s: str) -> bool:
+    def is_palindrome(self, s) -> bool:
         return s == s[::-1]
                 
             
