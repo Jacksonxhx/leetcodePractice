5297f7edaae134a18abdf9a4fa2cc0aeedb8e656
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-15 00:09:38 +0800

    Time: 220 ms (84.10%), Space: 19.3 MB (88.99%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,19 +1,31 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        '''
-        用前缀和和哈希表做
-        '''
-        pre_dic = {0: 1}
-        pre_sum = 0
-        cnt = 0
+        """
+        因为要避免超时，所以用前缀合+哈希表做
+        用哈希表统计不同前缀合出现的次数
+        """
+        # 初始化，前缀合0出现了1次
+        prefix_fre = {0:1}
+        
+        # 初始化前缀合
+        prefix_sum = 0
+        
+        # 初始化结果
+        res = 0
+        
+        # 遍历nums
         for num in nums:
-            pre_sum += num
-            # 统计有多少个前缀和为pre_sum - k
-            if pre_sum - k in pre_dic:
-                cnt += pre_dic[pre_sum - k]
-            # 收集所有的前缀合出现的次数
-            if pre_sum in pre_dic:
-                pre_dic[pre_sum] += 1
+            # 首先更新一直追踪的前缀和
+            prefix_sum += num
+            
+            # 然后处理base case
+            # 因为要找到和是k的subarray，就是找到当前num - k == prefix_sum的那一个hash存的数量，加到res上
+            if prefix_sum - k in prefix_fre:
+                res += prefix_fre[prefix_sum - k]
+            
+            if prefix_sum not in prefix_fre:
+                prefix_fre[prefix_sum] = 1
             else:
-                pre_dic[pre_sum] = 1
-        return cnt
+                prefix_fre[prefix_sum] += 1
+        
+        return res
