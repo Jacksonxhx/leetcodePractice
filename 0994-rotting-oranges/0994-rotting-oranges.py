4325a5a52466799d2dd40fa491644555de9423f2
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-15 17:55:34 +0800

    Time: 49 ms (65.10%), Space: 16.6 MB (52.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,47 +3,45 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         '''
-        BFS和队列做，核心思路是所有2橘子是一层，继续往下找
+        用BFS去做，通过记录数量来判断
+        核心思想就是，所有的2的橘子是一层，然后2往下找，2插入队列
         '''
-        
-        # 初始化
+        fresh_orange = 0
         queue = deque()
-        fresh_oranges = 0
         
-        # 遍历网格，初始化队列和新鲜橙子计数
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 2:
-                    queue.append((r, c))
-                elif grid[r][c] == 1:
-                    fresh_oranges += 1
-        
+        # 遍历整个grid，统计fresh_orange和插入2 to queue
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                elif grid[i][j] == 1:
+                    fresh_orange += 1
+                    
         # 如果没有新鲜橙子，直接返回0
-        if fresh_oranges == 0:
+        if fresh_orange == 0:
             return 0
         
-        # 计数和方向
-        minute = 0
+        # 初始化min记录
+        min_pass = 0
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         
-        # BFS
         while queue:
-            minute += 1
-            # 遍历一层的
+            min_pass += 1
+            
+            # 进入bfs
             for _ in range(len(queue)):
-                # 取第一个
-                x, y = queue.popleft()
+                x, y = queue.popleft()  
                 for dx, dy in directions:
-                    # 上下左右找新的
                     nx, ny = x + dx, y + dy
-                    # 判断
+                    
                     if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == 1:
                         grid[nx][ny] = 2
+                        fresh_orange -= 1
                         queue.append((nx, ny))
-                        fresh_oranges -= 1
             
-            # 当所有的好橘子都变了后
-            if fresh_oranges == 0:
-                return minute
+            # 结束条件
+            if fresh_orange == 0:
+                return min_pass
             
         return -1
+        
