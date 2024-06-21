6d6997b789e3530a8dd6c7b9fae5c6ad456c39c5
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-21 22:44:23 +0800

    Time: 82 ms (74.47%), Space: 17.8 MB (26.64%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -4,6 +4,7 @@ class Solution:
         滑动窗口做，固定长度len(p)
         用hash存p里的字数，然后遍历s滑动窗口统计
         """
+        # 构建p hash存判断标准
         need = collections.defaultdict(int)
         for ch in p:
             need[ch] += 1
@@ -13,15 +14,22 @@ class Solution:
         res = []
         left, right = 0, 0
         valid = 0
+        
         while right < len(s):
+            # 当right在need中
             if s[right] in need:
+                # window记录
                 window[s[right]] += 1
+                # 如果一个其中一个字符一样了 valid++
                 if window[s[right]] == need[s[right]]:
                     valid += 1
-
+            
+            # 当长度一样后
             if right - left + 1 >= window_size:
+                # 如果遇到答案
                 if valid == len(need):
                     res.append(left)
+                # 处理删除
                 if s[left] in need:
                     if window[s[left]] == need[s[left]]:
                         valid -= 1
