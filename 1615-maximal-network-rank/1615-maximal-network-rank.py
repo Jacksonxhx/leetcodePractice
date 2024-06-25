2091af3ed19074e14a72737c04f48a01eac83197
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-25 21:34:46 +0800

    Time: 253 ms (94.58%), Space: 18.4 MB (59.79%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -3,13 +3,19 @@ class Solution:
         """
         找最大的的入度pair
         找到之后判断是否相连，如果相链 max--
+        
+        用邻接矩阵提升效率
         """
         indegrees = {i: 0 for i in range(n)}
-        
+        adjacency = [[False] * n for _ in range(n)]
+                
         # 统计入度
         for road in roads:
-            indegrees[road[0]] += 1
-            indegrees[road[1]] += 1
+            u, v = road
+            indegrees[u] += 1
+            indegrees[v] += 1
+            adjacency[u][v] = True
+            adjacency[v][u] = True
         
         # 降序排
         sorted_indegrees = sorted(indegrees.items(), key=lambda x: x[1], reverse=True)
@@ -19,8 +25,11 @@ class Solution:
         for i in range(n):
             for j in range(i + 1, n):
                 u, v = sorted_indegrees[i][0], sorted_indegrees[j][0]
-                rank = sorted_indegrees[i][1] + sorted_indegrees[j][1]
-                if [u, v] in roads or [v, u] in roads:
+                if indegrees[u] + indegrees[v] < max_rank:
+                    # 如果当前组合的最大可能值已经小于max_rank，提前退出循环
+                    break
+                rank = indegrees[u] + indegrees[v]
+                if adjacency[u][v]:
                     rank -= 1
                 max_rank = max(max_rank, rank)
         
