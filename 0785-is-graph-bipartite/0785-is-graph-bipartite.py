7c1d3baf731f21968fab881e1e81ff353aeed71d
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-06-30 15:36:36 +0800

    Time: 132 ms (98.17%), Space: 16.9 MB (57.32%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,28 +1,27 @@
-from typing import List
-from collections import deque
-
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        # Initialize color array with -1 (uncolored)
-        color = [-1] * len(graph)
+        """
+        1. 用并查集，分完组后判断每一条边链接的两个node是否在一个set，若在，return False
         
-        # Iterate through each node in the graph
-        for i in range(len(graph)):
-            # If the node is not colored, perform BFS
-            if color[i] == -1:
-                # Start BFS from the node
-                queue = deque([i])
-                # Color the starting node with color 0
-                color[i] = 0
-                
-                while queue:
-                    node = queue.popleft()
-                    for neighbor in graph[node]:
-                        if color[neighbor] == -1:
-                            # Color the neighbor with opposite color
-                            color[neighbor] = 1 - color[node]
-                            queue.append(neighbor)
-                        elif color[neighbor] == color[node]:
-                            # If neighbor has the same color, it's not bipartite
-                            return False
+        2. 用DFS，从一个没有被标记的点出发，把跟他相邻的点标记到一个set，自己在另一个set，如果遇到冲突，则返回False
+        """
+        size = len(graph)
+        colors = [0 for _ in range(size)]
+        for i in range(len(colors)):
+            # 如果遇到了没有链接的node（colors[i] == 0）或者dfs时发现冲突了
+            if colors[i] == 0 and not self.dfs(graph, colors, i, 1):
+                return False 
+        return True
+    
+    def dfs(self, graph, colors, i, color):
+        colors[i] = color
+        # 遍历graph[i]的邻边
+        for j in graph[i]:
+            # 因为邻边的颜色肯定和自己不一样
+            if colors[j] == colors[i]:
+                return False
+            # 如果邻边没有，这次dfs传-color，要和上次不一样
+            # 这次从邻边开始找，dfs思想
+            if colors[j] == 0 and not self.dfs(graph, colors, j, -color):
+                return False
         return True
