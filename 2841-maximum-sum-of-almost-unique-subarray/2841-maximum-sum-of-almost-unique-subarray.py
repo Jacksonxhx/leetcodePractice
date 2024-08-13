2b0e72720ce98c51d144d061819ceda07b063284
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-08-13 23:26:42 +0800

    Time: 358 ms (46.81%), Space: 20.6 MB (56.60%) - LeetHub

--- 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.py
+++ 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.py
@@ -5,21 +5,33 @@ class Solution:
         使用哈希表来维护窗口内元素的频次，从而快速判断是否有至少 m 个不同的元素
         '''
         n = len(nums)
-        if k > n:  
+        if n < m:
             return 0
         
-        prefix = [0] * (n + 1)
-        for i in range(1, n + 1):
-            prefix[i] = prefix[i-1] + nums[i - 1]
-        
-        i, j = 0, k
+        freq = defaultdict(int)
+        distinct_count = 0
+        window_sum = 0
         res = float("-inf")
         
-        while j <= n:
-            if len(set(nums[i:j])) >= m:
-                res = max(res, prefix[j] - prefix[i])
-            i += 1
-            j = i + k
+        for i in range(n):
+            # add sum and freq
+            window_sum += nums[i]
+            freq[nums[i]] += 1
+            
+            # if distinct
+            if freq[nums[i]] == 1:
+                distinct_count += 1
+            
+            # window size exceed k
+            if i >= k:
+                window_sum -= nums[i - k]
+                freq[nums[i - k]] -= 1
+                
+                if freq[nums[i - k]] == 0:
+                    distinct_count -= 1
+            
+            if i >= k - 1 and distinct_count >= m:
+                res = max(res, window_sum)
         
         return res if res != float("-inf") else 0
         
