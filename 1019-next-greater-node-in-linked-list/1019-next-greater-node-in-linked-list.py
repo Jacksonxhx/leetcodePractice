efa029d2fa31902334f41cbf1225a9c8fbe827d0
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-08-18 17:32:39 +0800

    Time: 159 ms (72.42%), Space: 22.1 MB (6.83%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -5,21 +5,23 @@
 #         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-        walk = head
+        # 用单调栈去一直更新
         res = []
-        
-        while walk.next:
-            tmp_walk = walk.next
-            contain = False
-            while tmp_walk:
-                if tmp_walk.val > walk.val:
-                    res.append(tmp_walk.val)
-                    contain = True
-                    break
-                tmp_walk = tmp_walk.next
-            
-            if not contain:
-                res.append(0)
-            walk = walk.next
-            
-        return res + [0]
+        monostack = []
+        def f(node, i):
+            if node is None:
+                nonlocal res
+                # 从后往前，走到底开始initialization
+                res = [0] * i # i是链表长度
+                return
+            # 递归，相当于从后往左查
+            f(node.next, i + 1)
+            # 当栈顶的元素小于当前，把该元素pop掉
+            while monostack and monostack[-1] <= node.val:
+                monostack.pop()
+            if monostack:
+                # 每个栈顶剩下的就是比当前大的
+                res[i] = monostack[-1] 
+            monostack.append(node.val)
+        f(head, 0)
+        return res
