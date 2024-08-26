b0060614d7daf5f42561a03fc2a6f5e4f2c1157b
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-08-26 15:57:56 +0800

    Time: 46 ms (73.32%), Space: 16.8 MB (91.71%) - LeetHub

---
+++ 0893-groups-of-special-equivalent-strings/0893-groups-of-special-equivalent-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numSpecialEquivGroups(self, words: List[str]) -> int:
+        res = set()
+        for s in words:
+            sort_odd_even = ''.join(sorted(s[1::2]) + sorted(s[::2]))
+            res.add(sort_odd_even)
+        return len(res)
