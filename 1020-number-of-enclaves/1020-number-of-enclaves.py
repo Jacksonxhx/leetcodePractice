1deedf22d6b6d634781e2747d2ef6869c20c9376
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-09-04 19:48:55 -0400

    Time: 466 ms (68.13%), Space: 21.8 MB (80.63%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -5,37 +5,36 @@ class Solution:
         '''
         先统计有多少个1，然后遍历在boundary上的1，用visited记录
         '''
-        rows = len(grid)
-        cols = len(grid[0])
-        cnt = 0 
+        rows, cols = len(grid), len(grid[0])
         visited = [[False for _ in range(cols)] for _ in range(rows)]
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         
         queue = deque()
         
-        # 把所有在boundary的1加入，统计 1 个数
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 1:
-                    if row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
-                        queue.append((row, col))
-                                      
+        # 统计有多少个1
+        cnt = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    # 加入所有在boundary的1
+                    if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+                        queue.append((i, j))
+                    
                     cnt += 1
-        
-        tmp = 0
+    
+        # 用bfs找
         while queue:
-            row, col = queue.popleft()
-            if visited[row][col]:
-                continue
-                
-            visited[row][col] = True
-            tmp += 1
+            i, j = queue.pop()
             
+            if visited[i][j]:
+                continue
+            
+            visited[i][j] = True
+            cnt -= 1
             for dx, dy in directions:
-                nx, ny = row + dx, col + dy
+                nx, ny = i + dx, j + dy
                 
                 if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == 1 and not visited[nx][ny]:
                     queue.append((nx, ny))
-         
-        return cnt - tmp
-            
+        
+        return cnt
