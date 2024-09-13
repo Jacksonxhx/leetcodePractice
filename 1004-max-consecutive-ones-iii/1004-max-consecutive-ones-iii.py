e7f57a08a4626dca875e1d0e0910729d6e17a30b
Jackson <139523303+Jacksonxhx@users.noreply.github.com>
2024-09-13 00:16:53 -0400

    Time: 455 ms (24.50%), Space: 17.1 MB (14.80%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -4,26 +4,26 @@ class Solution:
         维护一个滑动窗口，这个窗口最多有k个0
         最后返回最大的窗口size
         '''
-        left, right, res = 0, 0, 0
-        # cnt记录有多少个0
-        cnt = 0
+        n = len(nums)
+        cnt_zero = 0
+        l, r = 0, 0
+        res = 0
         
-        while right < len(nums):
-            # 如果遇到0，cnt++
-            if nums[right] == 0:
-                cnt += 1
+        while r < n:
+            if nums[r] == 0:
+                cnt_zero += 1
+                
+            while cnt_zero > k:
+                if nums[l] == 0:
+                    cnt_zero -= 1
+                
+                l += 1
             
-            # 当cnt > k, left右移
-            while cnt > k:
-                if nums[left] == 0:
-                    cnt -= 1
-                left += 1
-            
-            res = max(res, right - left + 1)
-            
-            right += 1
-            
+            res = max(res, r - l + 1)
+            r += 1
+        
         return res
+        
                         
                 
             
